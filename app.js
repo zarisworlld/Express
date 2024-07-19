c4785c16a6b443b0a70f30ac2d317285bbc3ac0b
zaris world <zahrakheiri.eng@gmail.com>
2024-07-19 16:45:16 +0330

    contact api compeleted

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const sql = require('mssql');
-const bodyParser = require('body-parser');
-app.listen(port,(error)=>{
-    if(!error)
-        console.log("Successfully Listen");
-    else
-        console.log(`Error Occured While Listening to post${port}`);
+const sql = require("mssql");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+app.listen(port, (error) => {
+  if (!error) console.log(`Successfully Listen:${port}`);
+  else console.log(`Error Occured While Listening to post${port}`);
 });
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 const dbConfig = {
-    user: "sa",
-    password: "5544332211",
-    server: ".",
-    database: "Portfolio"
-}
-const executeQuery = function (res, query) {
-    sql.connect(dbConfig, function (err) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        }
-        else {
-            // create Request object
-            var request = new sql.Request();
-            // query to the database
-            request.query(query, function (err, result) {
-                if (err) {
-                    console.log(err);
-                    res.send(err);
-                }
-                else {
-                    res.send(result);
-                }
-            });
-        }
-    });
-}
+  user: "sa",
+  password: "5544332211",
+  server: "localhost",
+  database: "Portfolio",
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+const pool = new sql.ConnectionPool(dbConfig);
+const poolConnect = pool.connect();
 
- app.post('/contact',(req,res)=>{
-    var query = "INSERT INTO dbo.[Contact] (fullName, email,message,subject) VALUES (req.body.fullName, req.body.email, req.body.subject)";
-    executeQuery(res, query);
- });
+const executeQuery = async function (res, query, params) {
+  await poolConnect;
+  try {
+    const request = pool.request();
+    params.forEach((param) => {
+      request.input(param.name, param.type, param.value);
+    });
+    const result = await request.query(query);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+app.post("/contact", (req, res) => {
+  const query =
+    "INSERT INTO dbo.[contact] (fullName, email, subject, message) VALUES (@fullName, @Email, @Subject, @Message)";
+  const params = [
+    { name: "fullName", type: sql.NVarChar, value: req.body.fullName },
+    { name: "Email", type: sql.NVarChar, value: req.body.email },
+    { name: "Subject", type: sql.NVarChar, value: req.body.subject },
+    { name: "Message", type: sql.NVarChar, value: req.body.message },
+  ];
+  executeQuery(res, query, params);
+});
